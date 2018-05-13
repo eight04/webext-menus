d7d8a19ebb8d3d4acf002b559ab3d3fe85cd3afe
eight <eight04@gmail.com>
2018-05-13 19:33:28 +0800

    Release v0.1.3

--- dist/webext-menus.js
+++ dist/webext-menus.js
@@ -1,166 +1,164 @@
 var webextMenus = (function () {
-'use strict';
+	'use strict';
 
-/* eslint-env webextensions */
+	/* eslint-env webextensions */
 
-const MENUS = browser.menus || browser.contextMenus;
-const ALL_CONTEXTS_EXCLUDE = new Set([
-  "all",
-  "bookmark",
-  "browser_action",
-  "launcher",
-  "page_action",
-  "tab",
-  "tools_menu"
-]);
-const ALL_CONTEXTS = Object.values(MENUS.ContextType)
-  .filter(c => !ALL_CONTEXTS_EXCLUDE.has(c));
+	const MENUS = browser.menus || browser.contextMenus;
+	const ALL_CONTEXTS_EXCLUDE = new Set([
+	  "all",
+	  "bookmark",
+	  "browser_action",
+	  "launcher",
+	  "page_action",
+	  "tab",
+	  "tools_menu"
+	]);
+	const ALL_CONTEXTS = Object.values(MENUS.ContextType)
+	  .filter(c => !ALL_CONTEXTS_EXCLUDE.has(c));
 
-function webextMenus(menus) {
-	const ids = new Map;
-	const dynamicMenus = [];
-  const dynamicChecked = [];
-	
-	init(menus);
-	
-	function init(menus) {
-		const prevSibling = {};
+	function webextMenus(menus) {
+		const ids = new Map;
+		const dynamicMenus = [];
+	  const dynamicChecked = [];
 		
-		for (const menu of menus) {
-			// save id
-			if (menu.id != null) {
-				ids.set(menu.id, menu);
-			}
+		init(menus);
+		
+		function init(menus) {
+			const prevSibling = {};
 			
-			// raw options object for browser.menus.create
-			menu.options = Object.assign({}, menu);
-			
-      // mark as dynamic checked
-      if (typeof menu.checked === "function") {
-        delete menu.options.checked;
-        dynamicChecked.push(menu);
-      }
-			
-			// mark as dynamic
-			if (menu.oncontext) {
-        delete menu.options.oncontext;
-				dynamicMenus.push(menu);
-				menu.show = false;
-			} else {
-				create(menu);
-				menu.show = true;
-			}
-      
-			// build sibling relationship
-			for (const context of reduceContextType(getMenuContext(menu))) {
-				if (!prevSibling[context]) {
-					prevSibling[context] = new Map;
+			for (const menu of menus) {
+				// save id
+				if (menu.id != null) {
+					ids.set(menu.id, menu);
 				}
-				const prevCmd = prevSibling[context].get(menu.parentId);
-				if (prevCmd) {
-					if (!prevCmd.nextSibling) {
-						prevCmd.nextSibling = {};
+				
+				// raw options object for browser.menus.create
+				menu.options = Object.assign({}, menu);
+				
+	      // mark as dynamic checked
+	      if (typeof menu.checked === "function") {
+	        delete menu.options.checked;
+	        dynamicChecked.push(menu);
+	      }
+				
+				// mark as dynamic
+				if (menu.oncontext) {
+	        delete menu.options.oncontext;
+					dynamicMenus.push(menu);
+					menu.show = false;
+				} else {
+					create(menu);
+					menu.show = true;
+				}
+	      
+				// build sibling relationship
+				for (const context of reduceContextType(getMenuContext(menu))) {
+					if (!prevSibling[context]) {
+						prevSibling[context] = new Map;
 					}
-					prevCmd.nextSibling[context] = menu;
+					const prevCmd = prevSibling[context].get(menu.parentId);
+					if (prevCmd) {
+						if (!prevCmd.nextSibling) {
+							prevCmd.nextSibling = {};
+						}
+						prevCmd.nextSibling[context] = menu;
+					}
+					prevSibling[context].set(menu.parentId, menu);
 				}
-				prevSibling[context].set(menu.parentId, menu);
 			}
 		}
-	}
-	
-	function reduceContextType(contexts) {
-		return contexts.reduce((set, context) => {
-      if (context === "all") {
-        ALL_CONTEXTS.forEach(c => set.add(c));
-      } else {
-        set.add(context);
-      }
-			return set;
-		}, new Set);
-	}
-	
-	function getMenuContext(menu) {
-		if (menu.contexts) {
-			return menu.contexts;
+		
+		function reduceContextType(contexts) {
+			return contexts.reduce((set, context) => {
+	      if (context === "all") {
+	        ALL_CONTEXTS.forEach(c => set.add(c));
+	      } else {
+	        set.add(context);
+	      }
+				return set;
+			}, new Set);
 		}
-		if (menu.parentId != null) {
-			return getMenuContext(ids.get(menu.parentId));
+		
+		function getMenuContext(menu) {
+			if (menu.contexts) {
+				return menu.contexts;
+			}
+			if (menu.parentId != null) {
+				return getMenuContext(ids.get(menu.parentId));
+			}
+			return ["page"];
 		}
-		return ["page"];
-	}
-	
-	function create(menu) {
-		menu.id = MENUS.create(menu.options);
-		menu.isCreated = true;
-	}
-	
-	function destroy(menu) {
-		MENUS.remove(menu.id);
-		menu.isCreated = false;
-	}
-  
-  function update() {
-    updateShown();
-    updateChecked();
-  }
-	
-	function updateShown() {
-		const toShow = [];
-		for (const menu of dynamicMenus) {
-			const shouldShow = Boolean(menu.oncontext());
-			if (menu.show === shouldShow) continue;
-			
-			if (shouldShow) {
-				toShow.push(menu);
-				menu.show = true;
-			} else {
-				destroy(menu);
-				menu.show = false;
+		
+		function create(menu) {
+			menu.id = MENUS.create(Object.assign({}, menu.options));
+			menu.isCreated = true;
+		}
+		
+		function destroy(menu) {
+			MENUS.remove(menu.id);
+			menu.isCreated = false;
+		}
+	  
+	  function update() {
+	    updateShown();
+	    updateChecked();
+	  }
+		
+		function updateShown() {
+			const toShow = [];
+			for (const menu of dynamicMenus) {
+				const shouldShow = Boolean(menu.oncontext());
+				if (menu.show === shouldShow) continue;
+				
+				if (shouldShow) {
+					toShow.push(menu);
+					menu.show = true;
+				} else {
+					destroy(menu);
+					menu.show = false;
+				}
+			}
+			for (const menu of toShow) {
+				if (menu.isCreated) {
+					// already processed
+					continue;
+				}
+				destroySiblings(menu);
+				create(menu);
+				createSiblings(menu);
 			}
 		}
-		for (const menu of toShow) {
-			if (menu.isCreated) {
-				// already processed
-				continue;
+	  
+	  function updateChecked() {
+	    for (const menu of dynamicChecked) {
+	      MENUS.update(menu.id, {checked: menu.checked()});
+	    }
+	  }
+		
+		function destroySiblings(menu) {
+			if (!menu.nextSibling) return;
+			for (const nextMenu of Object.values(menu.nextSibling)) {
+				if (nextMenu.isCreated) {
+					destroy(nextMenu);
+				}
+				destroySiblings(nextMenu);
 			}
-			destroySiblings(menu);
-			create(menu);
-			createSiblings(menu);
 		}
-	}
-  
-  function updateChecked() {
-    for (const menu of dynamicChecked) {
-      MENUS.update(menu.id, {checked: menu.checked()});
-    }
-  }
-	
-	function destroySiblings(menu) {
-		if (!menu.nextSibling) return;
-		for (const nextMenu of Object.values(menu.nextSibling)) {
-			if (nextMenu.isCreated) {
-				destroy(nextMenu);
+		
+		function createSiblings(menu) {
+			if (!menu.nextSibling) return;
+			for (const nextMenu of Object.values(menu.nextSibling)) {
+				if (!nextMenu.isCreated && nextMenu.show) {
+					create(nextMenu);
+				}
+				createSiblings(nextMenu);
 			}
-			destroySiblings(nextMenu);
 		}
+		
+		return {update};
 	}
-	
-	function createSiblings(menu) {
-		if (!menu.nextSibling) return;
-		for (const nextMenu of Object.values(menu.nextSibling)) {
-			if (!nextMenu.isCreated && nextMenu.show) {
-				create(nextMenu);
-			}
-			createSiblings(nextMenu);
-		}
-	}
-	
-	return {update};
-}
 
-var webextMenus_1 = webextMenus;
-
-return webextMenus_1;
+	return webextMenus;
 
 }());
 //# sourceMappingURL=webext-menus.js.map
